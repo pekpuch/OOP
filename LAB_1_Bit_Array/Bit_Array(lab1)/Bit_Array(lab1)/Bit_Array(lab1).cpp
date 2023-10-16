baf14b8204fb907ee0f57b4296b80045d28b4d17
pekpuch <n.glazkov@g.nsu.ru>
2023-10-17 02:17:54 +0700

    Lab 1

---
+++ LAB_1_Bit_Array/Bit_Array(lab1)/Bit_Array(lab1)/Bit_Array(lab1).cpp
@@ -0,0 +1,27 @@
+﻿#include"Array.h"
+
+int main()
+{
+    // Создание и инициализация битового массива
+    BitArray bits(8, false);  // Создаем битовый массив из 8 битов
+    bits.set(1, true); // Устанавливаем первый бит в значение true
+    bits.set(2, true); // Устанавливаем третий бит в значение true
+    bits.set(4, true); // Устанавливаем пятый бит в значение true
+    std::cout << "BitArray: " << bits.to_string() << std::endl;
+
+    // Использование операторов
+    BitArray other(8, false);
+    other.set(0, true);
+    other.set(3, true);
+    other.set(5, true);
+
+    std::cout << "BitArray: " << other.to_string() << std::endl;
+
+
+    std::cout << "BitArray & other: " << (bits &= other).to_string() << std::endl; // Побитовое И
+    std::cout << "BitArray | other: " << (bits |= other).to_string() << std::endl; // Побитовое ИЛИ
+    std::cout << "BitArray ^ other: " << (bits ^= other).to_string() << std::endl; // Побитовое исключающее ИЛИ
+    std::cout << "~BitArray: " << (~bits).to_string() << std::endl;                 // Побитовое отрицание
+
+    return 0;
+}
