9ad61831e539dd03fa96c10a98efa8d2b6065259
pekpuch <n,glazkov@g.nsu.ru>
2024-03-01 14:08:47 +0700

    big_array

---
+++ big_array/build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/nikgl/OneDrive/Desktop/123/tests
+# Build directory: /mnt/c/Users/nikgl/OneDrive/Desktop/123/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/mnt/c/Users/nikgl/OneDrive/Desktop/123/build/tests/hello_test2[1]_include.cmake")
+subdirs("../_deps/googletest-build")
